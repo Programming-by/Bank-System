d4ccfb4f73cd15ceba054b6c303f3cc98d968039
AnasNaamneh <naamnih.anas@gmail.com>
2023-04-19 03:06:10 -0700

    Currency Main Screen

---
+++ clsCurrencyExchangeMainScreen.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <iostream>
+#include "clsScreen.h"
+
+class clsCurrencyExchangeScreen : protected clsScreen
+{
+private:
+
+	enum enCurrenciesMainMenueOptions { eListCurrencies = 1, eFindCurrency = 2, eUpdateRate = 3, eCurrencyCalculator = 4, eMainMenue = 5 };
+
+
+	static short ReadCurrenciesMainMenueOptions() {
+
+		cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 5]? ";
+
+		short Choice = clsInputValidate::ReadShortNumberBetween(1, 5, "Enter Number between 1 to 5? ");
+
+		return Choice;
+
+	}
+
+
+
+	static void _ShowCurrenciesListScreen() {
+
+		cout << "Currency List Screen Will be here";
+	}
+
+	static void _ShowFindCurrencyScreen() {
+		cout << "Currency Find Screen Will be here";
+
+	}
+
+	static void _ShowUpdateCurrencyRateScreen() {
+		cout << "Currency Update Rate Screen Will be here";
+
+	}
+
+	static void _ShowCurrencyCalculatorScreen() {
+
+		cout << "Currency Calculator  Screen Will be here";
+
+	}
+
+	static void _GoBackToCurrenciesMenue() {
+
+		cout << setw(37) << left << "" << "\n\tPress any key to go back to Currency Exchange Menue...\n";
+
+		system("pause>0");
+
+		ShowCurrenciesMenue();
+
+	}
+
+
+	static void _PerfromCurrenciesMainMenueOptions(enCurrenciesMainMenueOptions CurrenciesMainMenuOptions) {
+
+		switch (CurrenciesMainMenuOptions) {
+
+		case enCurrenciesMainMenueOptions::eListCurrencies:
+			system("cls");
+			_ShowCurrenciesListScreen();
+			_GoBackToCurrenciesMenue();
+			break;
+		case enCurrenciesMainMenueOptions::eFindCurrency:
+			system("cls");
+			_ShowFindCurrencyScreen();
+			_GoBackToCurrenciesMenue();
+			break;
+		case enCurrenciesMainMenueOptions::eUpdateRate:
+			system("cls");
+			_ShowUpdateCurrencyRateScreen();
+			_GoBackToCurrenciesMenue();
+			break;
+		case enCurrenciesMainMenueOptions::eCurrencyCalculator:
+			system("cls");
+			_ShowCurrencyCalculatorScreen();
+			_GoBackToCurrenciesMenue();
+			break;
+		case enCurrenciesMainMenueOptions::eMainMenue:
+
+			break;
+		}
+
+	};
+
+
+
+public:
+
+
+	static void ShowCurrenciesMenue() {
+
+		system("cls");
+
+
+		_DrawScreenHeader("\t Currency Exchange Main Screen");
+
+		cout << setw(37) << left << "" << "===========================================\n";
+		cout << setw(37) << left << "" << "\t\tCurrency Exchange Menue\n";
+		cout << setw(37) << left << "" << "===========================================\n";
+		cout << setw(37) << left << "" << "\t[1] List Currencies.\n";
+		cout << setw(37) << left << "" << "\t[2] Find Currency.\n";
+		cout << setw(37) << left << "" << "\t[3] Update Rate.\n";
+		cout << setw(37) << left << "" << "\t[4] Currency Calculator.\n";
+		cout << setw(37) << left << "" << "\t[5] Main Menue.\n";
+		cout << setw(37) << left << "" << "===========================================\n";
+
+
+		_PerfromCurrenciesMainMenueOptions((enCurrenciesMainMenueOptions)ReadCurrenciesMainMenueOptions());
+
+
+	}
+
+
+
+};
+
--- clsMainScreen.h
+++ clsMainScreen.h
@@ -11,6 +11,7 @@
 #include "clsTransactionsScreen.h"
 #include "clsManagaUsersScreen.h"
 #include "clsLoginRegisterScreen.h"
+#include "clsCurrencyExchangeMainScreen.h"
 #include "Global.h"
 using namespace std;
 
@@ -19,14 +20,14 @@ class clsMainScreen : protected clsScreen
 private:
 
 	enum enMainMenueOptions { eListClients = 1 , eAddNewClient = 2 , eDeleteClient
-	= 3 , eUpdateClient = 4 , eFindClient = 5 , eShowTransactionsMenue = 6, eManageUsers = 7, eLoginRegister = 8 , eExit = 9 };
+	= 3 , eUpdateClient = 4 , eFindClient = 5 , eShowTransactionsMenue = 6, eManageUsers = 7, eLoginRegister = 8 , eCurrencyExchange = 9 , eExit = 10 };
 
 
 	static short _ReadMainMenueOption() {
 
-		cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 9]? ";
+		cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 10]? ";
 
-		short Choice = clsInputValidate::ReadShortNumberBetween(1,9, "Enter Number between 1 to 9? ");
+		short Choice = clsInputValidate::ReadShortNumberBetween(1,10, "Enter Number between 1 to 10? ");
 
 		return Choice;
 
@@ -78,6 +79,10 @@ private:
 
 	static void _ShowLoginRegisterMenue() {
 		clsLoginRegisterScreen::ShowLoginRegisterMenue();
+	}
+
+	static void _ShowCurrencyExchangeMainScreen() {
+		clsCurrencyExchangeScreen::ShowCurrenciesMenue();
 	}
 
 	static void _Logout() {
@@ -132,6 +137,11 @@ private:
 			_ShowLoginRegisterMenue();
 			_GoBackToMainMenue();
 			break;
+		case enMainMenueOptions::eCurrencyExchange:
+			system("cls");
+			_ShowCurrencyExchangeMainScreen();
+			_GoBackToMainMenue();
+			break;
 		case enMainMenueOptions::eExit:
 			system("cls");
 			_Logout();
@@ -163,7 +173,8 @@ public:
 		cout << setw(37) << left << "" << "\t[6] Transactions.\n";
 		cout << setw(37) << left << "" << "\t[7] Manage Users.\n";
 		cout << setw(37) << left << "" << "\t[8] Login Register.\n";
-		cout << setw(37) << left << "" << "\t[9] Logout.\n";
+		cout << setw(37) << left << "" << "\t[9] Currency Exchange.\n";
+		cout << setw(37) << left << "" << "\t[10] Logout.\n";
 		cout << setw(37) << left << "" << "===========================================\n";
 
 
